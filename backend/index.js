f74e1ad2959aee9e8c15685863114ab9f460a47d
Neet <neetdhameliya321@gmail.com>
2024-06-29 13:17:10 +0530

    add: map

--- backend/index.js
+++ backend/index.js
@@ -14,40 +14,47 @@ const router = require("./route/api");
 require("./config/passport");
 
 // middlewares
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: "http://localhost:5173", credentials: true }));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: false
-}));
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+  })
+);
 
 app.use(passport.initialize()); // for passport
 app.use(passport.session());
 
-
-app.use((err, req, res, next) => { // for server error
+app.use((err, req, res, next) => {
+  // for server error
   console.error(err.stack);
-  res.status(500).json({ message: 'Internal Server Error' });
+  res.status(500).json({ message: "Internal Server Error" });
 });
 
-app.use('/api', router); // for routing
+app.use("/api", router); // for routing
 
 // two routes for authentication purpose
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get('/auth/google/callback',
-    passport.authenticate('google', { failureRedirect: '/login', session: true }),
-    (req, res) => {
-        res.redirect('http://localhost:5173/home'); // Redirect to your frontend app
-    }
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile", "email"] })
 );
 
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", { failureRedirect: "/login", session: true }),
+  (req, res) => {
+    res.redirect("http://localhost:5173/home"); // Redirect to your frontend app
+  }
+);
 
-
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Home Route" });
+});
 
 const port = process.env.PORT || 3000;
 
